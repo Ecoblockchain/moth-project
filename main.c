9e2190c7d6644fde0fdff07f4fc1c9e52e9505af
James Vaughan <james@jamesbvaughan.com>
2015-07-04 22:06:57 -0700

    first commit

---
+++ main.c
@@ -0,0 +1,137 @@
+#include <stdio.h>
+#include <time.h>
+#include <fcntl.h>
+#include <termios.h>
+#include <unistd.h>
+#include <string.h>
+
+volatile int tty_fd_gps;
+
+// used when checking validity of NMEA sentences
+void btoh(unsigned char c, char *str){
+	unsigned char x;
+	x = c >> 4;
+	str[0] = x + (x > 9? 'A'-10: '0');
+	x = c & 0x0f;
+	str[1] = x + ( x > 9? 'A' - 10 : '0');
+	str[2] = '\0';
+}
+
+// used to verify the validity of NMEA sentences
+int verify_nmea(char *string){
+	if (string[0] != '$')
+		return -1;
+	int length = strlen(string);
+	if (length > 90) {
+		printf("ER string  %s too long\n",string);
+		return -1;
+	}
+	if (string[length-1]!= '\n') {
+		printf("ER no newline\n");
+		return -1;
+	}
+	if (string[length-2] != '\r') {
+		printf("ER no return\n");
+		return -1;
+	}
+	int p = 1; // start after the $, stop before the *
+	char temp[3];
+	char cs[3];
+	cs[2] = '\0';
+	cs[0] = string[length-4];
+	cs[1] = string[length-3];
+	temp[2]='\0';
+	temp[1] = '\0';
+	char c = '\0';
+	while (string[p] != '*') {
+		c = c ^ string[p++];
+	}
+	// compare the checksums
+	btoh(c,temp);
+	p = strcmp(cs,temp);
+	if (p != 0) {
+		printf("ER CHECKSUM ERROR in %s, %s != %s\n",string, cs, temp);
+		return -1;
+	}
+
+	return 0;
+}
+
+// initialize the gps
+void initGps() {
+	tty_fd_gps = open("/dev/ttyMFD1", O_RDWR);
+	struct termios tio;
+	tcgetattr(tty_fd_gps, &tio);
+	cfsetspeed(&tio, B9600);
+	cfmakeraw(&tio);
+	tcsetattr(tty_fd_gps, TCSANOW, &tio);
+}
+
+// read from the gps forever
+void gpsRead() {
+	printf("ST gps read is running\n");
+	char aa;
+	char local_buffer[500];
+	int idx = 0;
+	initGps();
+	int read_more = 0;
+
+	time_t rawtime;
+	char * timeString;
+
+	time_t start = time(0);
+
+	FILE * logfile = fopen("data.log", "w");
+	if (logfile == NULL) {
+		printf("Error opening logfile.\n");
+		return;
+	}
+	fputs("BEGIN:", logfile);
+
+	while (1) {
+		while (read(tty_fd_gps, &aa, 1) == -1 || (aa != '$' && read_more != 1) );   // read 1 character from stream (blocking call)
+		read_more = 1;
+		if (aa != '\n') {
+			local_buffer[idx++] = aa;
+			if (idx > 400) {
+				// prevent segmentation faults
+				idx = 0; 
+				printf("\nGR ***********************************************\nlocal_buffer overflow in read nmea.\n Connect inputs and Cycle power to recover\n*****************************************\n");
+				int flag = 1;
+				char old = 'x';
+				while (flag == 1) {
+					read(tty_fd_gps, &aa, 1);
+					if (aa == '\n'  && old == '\r') flag = 0;
+					old = aa;
+				}
+			}
+		} else {
+			// at end of sentence
+			read_more = 0;
+			local_buffer[idx] = '\n';
+			local_buffer[idx + 1] = '\0';
+			idx = 0;
+			if(verify_nmea(local_buffer) == 0) {
+				if (strstr(local_buffer,",,,,,") && strstr(local_buffer, "RMC")){
+					printf("ST GPS Not Ready %s\n",local_buffer);
+				} else if (strstr(local_buffer, "RMC")) {
+					// GPS Sentence
+					rawtime = time(0);					
+					timeString = ctime(&rawtime);
+					timeString[24] = '\0';
+					printf("%s: GR %s", timeString, local_buffer);
+					fprintf(logfile, "%s: GR %s", timeString, local_buffer);
+					while (difftime(time(0), start) < 1);
+					start = time(0);
+				}
+			}
+		}
+	}
+
+	fclose(logfile);
+}
+
+
+int main() {
+	gpsRead();
+}
