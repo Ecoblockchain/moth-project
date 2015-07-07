7e8de578be0f2e66e5d07ec658b715ae5c45dcab
James Vaughan <james@jamesbvaughan.com>
2015-07-07 13:37:54 -0700

    added newlines

--- src/stringpot.c
+++ src/stringpot.c
@@ -7,14 +7,14 @@ void * spRead() {
 
 	stringpot = mraa_aio_init(0);
 	if (stringpot == NULL) {
-		printf("Error initializing String Potentiometer");
+		printf("Error initializing String Potentiometer\n");
 		return NULL;
 	}
-	printf("Initialized String Potentiometer");
+	printf("Initialized String Potentiometer\n");
 
 	while(1) {
 		spValue = mraa_aio_read_float(stringpot);
-		printf("String Pot Value: %f", spValue);
+		printf("String Pot Value: %f\n", spValue);
 	}
 
 	mraa_aio_close(stringpot);
