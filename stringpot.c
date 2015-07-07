a44e252f64a733fe5315bd3c34a90baa7ec1f07d
James Vaughan <james@jamesbvaughan.com>
2015-07-06 18:14:49 -0700

    added stringpot code

--- stringpot.c
+++ stringpot.c
@@ -1,6 +1,20 @@
 #include <mraa/aio.h>
 
-void initStringPot() {
+void * spRead() {
+	mraa_aio_context stringpot;
+	float spValue;
 
+	stringpot = mraa_aio_init(0);
+	if (stringpot == NULL) {
+		printf("Error initializing String Potentiometer");
+		return NULL;
+	}
+
+	while(1) {
+		spValue = mraa_aio_read_float(stringpot);
+		printf("String Pot Value: %f", spValue);
+	}
+
+	mraa_aio_close(stringpot);
+	return NULL;
 }
-	
