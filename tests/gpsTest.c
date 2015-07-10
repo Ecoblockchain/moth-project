c6adfbe3d9bedb43bae96dfec6786890c04d5131
James Vaughan <james@jamesbvaughan.com>
2015-07-10 03:16:00 +0000

    finalized gpsTest

--- tests/gpsTest.c
+++ tests/gpsTest.c
@@ -5,6 +5,7 @@
 
 volatile int gps;
 
+// Initialize GPS and set baud rate
 void initGps(){
 	gps = open("/dev/ttyMFD1", O_RDWR);
 	struct termios tio;
@@ -16,24 +17,11 @@ void initGps(){
 
 int main() {
 	initGps();
-	/* char *string = "$PMTK104*37\r\n"; */
-	/* if (write(gps, string, 13) == -1) { */
-	/* 	printf("error writing to gps\n"); */
-	/* 	return 1; */
-	/* } */
-
-	char * c;
+	char *c;
+	
+	// Continuously read in and print out data
 	while (1) {
 		while (read(gps, c, 1) == -1);
-			printf("READ: %c\n", c);
+		printf("%c", *c);
 	}
-
-	if (close(gps) == -1) {
-		printf("error closing gps\n");
-		return 1;
-	}
-	/* printf("success writing to gps\n"); */
-
-	printf("Closed GPS connection\n");	
-	return 0;
 }
