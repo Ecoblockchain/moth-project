07d92f828a83e6ace9e692298f2d0c6138d3d73c
James Vaughan <james@jamesbvaughan.com>
2015-07-19 17:12:23 -0700

    added more prototypes

--- src/prototypes.h
+++ src/prototypes.h
@@ -8,20 +8,11 @@
 #ifndef PROTOTYPES_H_
 #define PROTOTYPES_H_
 int create_nmea(char*);
-int gx_display(int display, char*, char*, char*, char*, char*, char*);
-int gx_display_new(int, int, char*,char*, char*, char*, char*, char*, char*, char*, char*, char*, char*, char*);
-int read_stowe_key_code();
 void init_tty();
 void sendLine(char*);
 void sendNmea(char*);
 double get_heel();
-int init_heel();
 int str_split(char**, char*, char*, int);
-int init_bluetooth();
-int ping_bluetooth();
-int restore_bluetooth();
-int write_bluetooth(char*);
-void process_blue(char*);
 void enter_test_mode(char**);
 void exit_text_mode();
 void init_cal_array_defaults();
@@ -34,8 +25,6 @@ double read_from_cal_array(int);
 int str_split(char **, char *, char *, int );
 void load_cal_name();
 void nmea_write(char*);
-void display_stowe_3(char*);
-void send_data_to_bluetooth(int);
 int verify_nmea(char *);
 
 // log file
@@ -43,21 +32,9 @@ void parse_rmb(char*);
 void parse_rmc(char*);
 void save_log_value(int, double);
 void write_log();
+void log_error(char *message);
 
-// current related
-double normalize(double);
-void get_current (double, double, double*);
-
-void write_lcd(char*, int, int, char*, int, int);
-int init_lcd();
-
-void send_debug_data_to_bluetooth();
-
-void vector_add(double*, double*, double*);
-void vector_sub(double*, double*, double*);
-
-
-// James
+// main
 void * gpsRead();
 void * spRead();
 void * statusLED();
