91397c7eb794797bb85d4d8e4b2808bde62cc042
James Vaughan <james@jamesbvaughan.com>
2015-07-19 17:12:44 -0700

    first draft of adc code

--- src/stringpot.c
+++ src/stringpot.c
@@ -6,32 +6,36 @@
 
 #include <unistd.h>
 #include <mraa/spi.h>
+#include <mraa/gpio.h>
 #include <stdio.h>
+#include "prototypes.h"
 
 void * spRead() {
+	const int spi_dev_num = 5;
+	int chipSelectPin = 8;
 	mraa_spi_context spi;
-	spi = mraa_spi_init(0);
-	unsigned int response = 0;
+	mraa_gpio_context chipSelect;
+
+	spi = mraa_spi_init(spi_dev_num);
+	chipSelect = mraa_gpio_init(chipSelectPin);
+	mraa_gpio_mode(chipSelect, MRAA_GPIO_OUT_HIGH);
 	printf("Initialized SPI\n");
-	uint8_t data[] = {0x00, 100};
+
+	unsigned int response = 0;
+	uint8_t data[] = {0x68};
+	uint8_t zero[] = {0x00};
 	uint8_t * recv;
 
 	while (1) {
-		int i;
-		for (i = 90; i < 130; i++) {
-			data[1] = i;
-			recv = mraa_spi_write_buf(spi, data, 2);
-			printf("Writing -%i", i);
-			printf("RECIEVED-%i-%i\n", recv[0], recv[1]);
-			usleep(100000);
-		}
-		for (i = 130; i < 90; i--) {
-			data[1] = i;
-			recv = mraa_spi_write_buf(spi, data, 2);
-			printf("Writing -%i", i);
-			printf("RECIEVED-%i-%i\n", recv[0], recv[1]);
-			usleep(100000);
-		}
+		mraa_gpio_write(chipSelect, 0);		
+		recv = mraa_spi_write_buf(spi, data, 1);
+		response = recv[0];
+		response = response & 0x03;
+		response = response << 8;
+		recv = mraa_spi_write_buf(spi, zero, 1);
+		response = response | recv[0];
+		printf("value: %i", response);
+		usleep(50000);
 	}
 
 	return NULL;
