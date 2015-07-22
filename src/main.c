49e1d67a95945cf8b8cc607eb292904e6117d566
James Vaughan <james@jamesbvaughan.com>
2015-07-22 16:54:17 -0700

    joined thread

--- src/main.c
+++ src/main.c
@@ -13,6 +13,11 @@ int main() {
 	pthread_t thread;
 
 	pthread_create(&thread, NULL, gpsRead, NULL);
+	pthread_join(thread, NULL);
 
+	/* int i; */
+	/* for (i = 0; i < MAX_THREADS; i++) { */
+	/* 	pthread_join(threads[i], NULL); */
+	/* } */
 	return 0;
 }
