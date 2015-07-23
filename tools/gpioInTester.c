9b9431d7b93e636b59cb79b333c5cac0c1facd43
James Vaughan <james@jamesbvaughan.com>
2015-07-23 01:56:39 -0700

    upgraded gpio tester

--- tools/gpioInTester.c
+++ tools/gpioInTester.c
@@ -5,7 +5,12 @@ int main() {
   mraa_gpio_context pin;
   pin = mraa_gpio_init(45);
   mraa_gpio_dir(pin, MRAA_GPIO_IN);
+  int status = mraa_gpio_read(pin);
+  int otherStatus;
   while (1) {
-    printf("%i\n", mraa_gpio_read(pin));
+    otherStatus = mraa_gpio_read(pin);
+    if (status != otherStatus)
+      printf("Change detected!\n");
+    status = mraa_gpio_read(pin);
   }
 }
