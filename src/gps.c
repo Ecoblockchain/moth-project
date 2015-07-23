ab953a186a61ea8861f0a743b55cdc33d6ce4a16
James Vaughan <james@jamesbvaughan.com>
2015-07-22 17:26:23 -0700

    new code

--- src/gps.c
+++ src/gps.c
@@ -73,14 +73,14 @@ void initGps() {
 	tcsetattr(tty_fd_gps, TCSANOW, &tio);
 	char baudSetting[20];
 	sprintf(baudSetting, "$PMTK251,115200*1F\r\n");
-	write(tty_fd_gps, baudSetting, 20);
+	//write(tty_fd_gps, baudSetting, 20);
 	tcgetattr(tty_fd_gps, &tio);
 	cfsetspeed(&tio, B115200);
 	cfmakeraw(&tio);
 	tcsetattr(tty_fd_gps, TCSADRAIN, &tio);
 	char rateSetting[51];
 	sprintf(rateSetting, "$PMTK314,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*29\r\n");
-	write(tty_fd_gps, rateSetting, 51);
+	//write(tty_fd_gps, rateSetting, 51);
 	char freqSetting[17];
 	sprintf(freqSetting, "$PMTK220,100*2F\r\n");
 	write(tty_fd_gps, rateSetting, 17);
