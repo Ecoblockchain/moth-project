f90c3d99ef6c75e359b4979136b51e5c32241df4
James Vaughan <james@jamesbvaughan.com>
2015-07-22 22:33:21 -0700

    added base button functionality

--- src/main.c
+++ src/main.c
@@ -9,13 +9,35 @@
 
 #define MAX_THREADS 2
 
-int main() {
-	pthread_t gpsThread;
-	pthread_t sonarThread;
+pthread_t gpsThread;
+pthread_t sonarThread;
+
+void startAll() {
 	pthread_create(&gpsThread, NULL, gpsRead, NULL);
 	pthread_create(&sonarThread, NULL, sonarRead, NULL);
-	pthread_join(gpsThread, NULL);
-	pthread_join(sonarThread, NULL);
+}
+
+void cancelAll() {
+	pthread_cancel(gpsThread);
+	pthread_cancel(sonarThread);
+}
+
+int main() {
+	int running = 0;
+
+	while (1) {
+		if (1) { // start/stop button
+			if (!running) {
+				startAll();
+				running = 1;
+			}
+		} else {
+			if (running) {
+				cancelAll();
+				running = 0;
+			}
+		}
+	}
 
 	return 0;
 }
