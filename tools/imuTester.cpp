387de3a0ec8342388c3278c8bd5e6f01767fb070
James Vaughan <james@jamesbvaughan.com>
2015-07-23 20:00:21 -0700

    now it's just the compass

--- tools/imuTester.cpp
+++ tools/imuTester.cpp
@@ -6,19 +6,22 @@
 int main() {
     int16_t *raw;
     int16_t *rot;
+    int16_t *pos;
     float *acc;
     float *ang;
 
     //upm::Adxl345* accel = new upm::Adxl345(6);
-    upm::Itg3200* gyro = new upm::Itg3200(6);
+    //upm::Itg3200* gyro = new upm::Itg3200(6);
+    upm::Hmc5883l* compass = new upm::Hmc5883l(1);
 
     while(true){
         //accel->update(); // Update the data
-        gyro->update(); // Update the data
+        //gyro->update(); // Update the data
         //raw = accel->getRawValues(); // Read raw sensor data
         //acc = accel->getAcceleration(); // Read acceleration (g)
-        rot = gyro->getRawValues();
-        ang = gyro->getRotation();
+        //rot = gyro->getRawValues();
+        //ang = gyro->getRotation();
+        pos = compass->coordinates();
 
         //printf("Current scale: 0x%2xg\t", accel->getScale());
         //printf("Raw: %6d %6d %6d\t", raw[0], raw[1], raw[2]);
@@ -26,12 +29,15 @@ int main() {
         //printf("AccY: %5.2f g\t", acc[1]);
         //printf("AccZ: %5.2f g\n", acc[2]);
 
-        printf("Raw: %6d %6d %6d\t", rot[0], rot[1], rot[2]);
-        printf("AngX: %5.2f\t", ang[0]);
-        printf("AngY: %5.2f\t", ang[1]);
-        printf("AngZ: %5.2f\t", ang[2]);
-        printf("Temp: %5.2f Raw: %6d\t", gyro->getTemperature(), gyro->getRawTemp());
-      
+        //printf("Raw: %6d %6d %6d\t", rot[0], rot[1], rot[2]);
+        //printf("AngX: %5.2f\t", ang[0]);
+        //printf("AngY: %5.2f\t", ang[1]);
+        //printf("AngZ: %5.2f\t", ang[2]);
+        //printf("Temp: %5.2f Raw: %6d\t", gyro->getTemperature(), gyro->getRawTemp());
+
+        fprintf(stdout, "coor: %5d %5d %5d ", pos[0], pos[1], pos[2]);
+        fprintf(stdout, "heading: %5.2f direction: %3.2f\n\n", compass->heading(), compass->direction());
+
 
         usleep(100000);
     }
