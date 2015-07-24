0edf62dcc3b713636dcb6d450b0c37874aa9c82e
James Vaughan <james@jamesbvaughan.com>
2015-07-24 03:00:28 -0700

    commented stuff out

--- src/sonar.c
+++ src/sonar.c
@@ -6,25 +6,25 @@
 
 uint8_t sonar[] = {0x02, 0x04, 0x00, 0x00};
 int arrayValues[] = {SONAR_1, SONAR_2, SONAR_3, SONAR_4};
-mraa_i2c_context i2c;
+mraa_i2c_context i2c_sonar;
 
 void pingSonar(int id) {
-  mraa_i2c_address(i2c, sonar[id]);
-  mraa_i2c_write_byte(i2c, 0x51);
+  mraa_i2c_address(i2c_sonar, sonar[id]);
+  mraa_i2c_write_byte(i2c_sonar, 0x51);
 }
 
 void updateSonar(int id) {
   uint8_t buf[2];
   int value;
-  mraa_i2c_address(i2c, sonar[id]);
-  mraa_i2c_read(i2c, buf, 2);
+  mraa_i2c_address(i2c_sonar, sonar[id]);
+  mraa_i2c_read(i2c_sonar, buf, 2);
   value =  (buf[0] << 8) | buf[1];
   parseSonar(arrayValues[id], value);
 }
 
 void* sonarRead() {
-	i2c = mraa_i2c_init(1);
-  mraa_i2c_frequency(i2c, MRAA_I2C_STD);
+	i2c_sonar = mraa_i2c_init(1);
+  mraa_i2c_frequency(i2c_sonar, MRAA_I2C_STD);
 
   pingSonar(0);
   usleep(40000);
